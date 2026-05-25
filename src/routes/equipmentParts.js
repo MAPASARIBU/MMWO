23798ad034f49d378d8288d530868f1f44ad7a66
MUHAMMAD ALI PASARIBU <Alipasaribu873@gmail.com>
2026-05-25 20:49:38 +0700

    Fix: Allow MTC and PROC roles to add/edit/delete parts

--- src/routes/equipmentParts.js
+++ src/routes/equipmentParts.js
@@ -6,10 +6,10 @@ const { ensureAuthenticated, ensureRole } = require('../middleware/authMiddlewar
 
 // Part routes
 router.get('/:equipmentId/parts', ensureAuthenticated, equipmentPartsController.getParts);
-router.post('/:equipmentId/parts', ensureRole(['ADMIN']), equipmentPartsController.createPart);
-router.post('/:equipmentId/parts/:partId/replace', ensureRole(['ADMIN']), equipmentPartsController.replacePart);
-router.put('/:equipmentId/parts/:partId', ensureRole(['ADMIN']), equipmentPartsController.editPart);
-router.delete('/:equipmentId/parts/:partId', ensureRole(['ADMIN']), equipmentPartsController.deletePart);
+router.post('/:equipmentId/parts', ensureRole(['ADMIN', 'MTC', 'PROC', 'SPV', 'MANAGER']), equipmentPartsController.createPart);
+router.post('/:equipmentId/parts/:partId/replace', ensureRole(['ADMIN', 'MTC', 'PROC', 'SPV', 'MANAGER']), equipmentPartsController.replacePart);
+router.put('/:equipmentId/parts/:partId', ensureRole(['ADMIN', 'MTC', 'PROC', 'SPV', 'MANAGER']), equipmentPartsController.editPart);
+router.delete('/:equipmentId/parts/:partId', ensureRole(['ADMIN', 'MTC', 'PROC', 'SPV', 'MANAGER']), equipmentPartsController.deletePart);
 
 // HM Record routes
 router.get('/:equipmentId/hm', ensureAuthenticated, equipmentPartsController.getHMRecords);
