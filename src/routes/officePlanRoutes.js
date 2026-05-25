790d1442db1547f89585d3bfc554315f94be1301
MUHAMMAD ALI PASARIBU <Alipasaribu873@gmail.com>
2026-05-25 18:18:37 +0700

    Fix: requireRole to ensureRole in officePlanRoutes

--- src/routes/officePlanRoutes.js
+++ src/routes/officePlanRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const officePlanController = require('../controllers/officePlanController');
-const { requireRole } = require('../middleware/authMiddleware');
+const { ensureRole } = require('../middleware/authMiddleware');
 
-router.post('/', requireRole(['ADMIN', 'MANAGER', 'SENIOR_MANAGER', 'OAA']), officePlanController.createOfficePlan);
-router.post('/bulk-create', requireRole(['ADMIN', 'MANAGER', 'SENIOR_MANAGER', 'OAA']), officePlanController.bulkCreateOfficeWOs);
-router.put('/:id', requireRole(['ADMIN', 'MANAGER', 'SENIOR_MANAGER', 'OAA']), officePlanController.editOfficePlan);
-router.delete('/:id', requireRole(['ADMIN', 'MANAGER', 'SENIOR_MANAGER', 'OAA']), officePlanController.deleteOfficePlan);
+router.post('/', ensureRole(['ADMIN', 'MANAGER', 'SENIOR_MANAGER', 'OAA']), officePlanController.createOfficePlan);
+router.post('/bulk-create', ensureRole(['ADMIN', 'MANAGER', 'SENIOR_MANAGER', 'OAA']), officePlanController.bulkCreateOfficeWOs);
+router.put('/:id', ensureRole(['ADMIN', 'MANAGER', 'SENIOR_MANAGER', 'OAA']), officePlanController.editOfficePlan);
+router.delete('/:id', ensureRole(['ADMIN', 'MANAGER', 'SENIOR_MANAGER', 'OAA']), officePlanController.deleteOfficePlan);
 
 module.exports = router;
